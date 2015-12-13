8ae6b23b7439517b8fbe81738b17558ffdce86f1
Dvacher <kud@mm.st>
2015-12-13 20:46:21 +0200

    add Promises chaning for forecast and current weather icons

--- js/main.js
+++ js/main.js
@@ -2,23 +2,32 @@
 
     var coordinates;
     var forecast;
-    var weatherIcons = fetchJSONFile("https://gist.githubusercontent.com/tbranyen/62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json", function(data) { weatherIcons = data; });
+    var weatherIcons;// = getJSON("https://gist.githubusercontent.com/tbranyen/62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json");
 
-    function fetchJSONFile(path, callback) {
-        
-        var httpRequest = new XMLHttpRequest();
+    function get(url) {
 
-        httpRequest.onreadystatechange = function() {
-            if (httpRequest.readyState === 4) {
-                if (httpRequest.status === 200) {
-                    var data = JSON.parse(httpRequest.responseText);
-                    if (callback) callback(data);
+        return new Promise(function(resolve, reject) {
+            
+            var req = new XMLHttpRequest();
+            
+            req.onreadystatechange = function() {
+                if (req.readyState === 4) {
+                    if (req.status === 200) {
+                        resolve(req.responseText);
+                    } else {
+                        reject(Error(req.statusText));
+                    }
                 }
-            }
-        };
+            };
+            
+            // Handle network errors
+            req.addEventListener("error", function() {
+                reject(Error("Network Error"));
+            });
 
-        httpRequest.open('GET', path);
-        httpRequest.send(); 
+            req.open('GET', url);
+            req.send();
+        });
     }
 
     function getWeather() {
@@ -29,23 +38,32 @@
             return; 
         }
 
-        //get JSON once coordinates are ready
-        fetchJSONFile("http://api.openweathermap.org/data/2.5/weather?lat=" + coordinates.lat + "&lon=" + coordinates.lon + "&units=metric&appid=3ee018281bf1fd7192d1cc78e0e3e24e", function(data) {
+        //get Open Weather JSON once coordinates are ready
+        get("http://api.openweathermap.org/data/2.5/weather?lat=" + coordinates.lat + "&lon=" + coordinates.lon + "&units=metric&appid=3ee018281bf1fd7192d1cc78e0e3e24e").then(function(data) {
 
-            forecast = data;
+            forecast = JSON.parse(data);
             document.querySelector(".location").textContent  = forecast.name;
             document.querySelector(".description").textContent  = forecast.weather[0].description;
             document.querySelector(".wind").innerHTML = forecast.wind.speed + " km/h; <br> direction: " + forecast.wind.deg + " degrees";
             document.querySelector("#temp").textContent  = forecast.main.temp + " °C";
-
+            
+        }).then(function() {
+            
+            // get weather icon 
+            get("https://gist.githubusercontent.com/tbranyen/62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json").then(function(data) {
+                weatherIcons = JSON.parse(data);
+                showWeatherIcon();
+            });
         });
     }
 
     function showWeatherIcon() {
         
-        var prefix = 'wi wi-';
-        var code = forecast.weather[0].id;
-        var icon = weatherIcons[code].icon;
+        //https://gist.github.com/tbranyen/62d974681dea8ee0caa1
+
+        var prefix = 'wi wi-',
+            code = forecast.weather[0].id,
+            icon = weatherIcons[code].icon;
 
         // If we are not in the ranges mentioned above, add a day/night prefix.
         if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
@@ -55,6 +73,7 @@
         // Finally tack on the prefix.
         icon = prefix + icon;
         console.log(icon);
+        document.querySelector("#weather_icon").className = icon;
     }
 
     window.addEventListener("load", function() {
@@ -88,7 +107,7 @@
     });
 
     getWeather();
-    showWeatherIcon();
+    //  showWeatherIcon();
 
 // }
 
